3aac23ab35af2a22f0d1c102c1afb4731693db21
kingmingseo <multi1971@mju.ac.kr>
2023-01-22 01:08:31 +0900

    기능개발

--- Baekjoon/kingmingseo/hello.py
+++ Baekjoon/kingmingseo/hello.py
@@ -1,5 +1,25 @@
-from collections import deque
+import math
 
-deq=deque()
 
-print(deq)
+def solution(progresses, speeds):
+    temp = []
+    temp2 = []
+    answer = [0]
+
+    for i in progresses:
+        temp.append(100 - i)
+
+    for j in range(len(progresses)):
+        temp2.append(math.ceil(temp[j] / speeds[j]))
+
+    max2 = temp2[0]
+    for x in range (len(temp2)):
+        if(temp2[x] > max2):
+            max2 = temp2[x]
+            answer.append(1)
+        else:
+            answer[-1]=answer[-1]+1
+
+    return answer
+
+
