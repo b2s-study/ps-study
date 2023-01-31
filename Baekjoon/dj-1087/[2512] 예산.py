c02d24043ee164734e6700f421ec6f5b253bdd96
Dongjun Jeong <sam1087@mju.ac.kr>
2023-01-31 14:02:42 +0900

    [2512] 예산 (해결)

--- Baekjoon/dj-1087/[2512] 예산.py
+++ Baekjoon/dj-1087/[2512] 예산.py
@@ -1,5 +1,3 @@
-# 이슈사항 - 시간 초과
-
 import sys
 
 input = sys.stdin.readline
@@ -8,13 +6,12 @@ N = int(input())
 budget_list = list(map(int, input().split()))
 limit = int(input())
 
-budget_list.sort()
-min_budget, max_budget = budget_list[0], budget_list[-1]
+min_budget, max_budget = 0, max(budget_list)
 while (True):
+    mid_budget = (min_budget + max_budget) // 2
     if min_budget > max_budget:
         break
 
-    mid_budget = (min_budget + max_budget) / 2
     total_budget = 0
     for budget in budget_list:
         if budget < mid_budget:
@@ -23,33 +20,10 @@ while (True):
             total_budget += mid_budget
 
     if total_budget <= limit:
-        min_budget += 1
+        min_budget = mid_budget + 1
+    elif total_budget > limit:
+        max_budget = mid_budget - 1
     else:
-        max_budget -= 1
+        break
 
-# result = int((min_budget + max_budget) / 2)
-print(max_budget)
-
-
-# 블로그 정답 코드
-# input = sys.stdin.readline
-
-# N = int(input())
-# cities = list(map(int, input().split()))
-# budgets = int(input())  # 예산
-# start, end = 0, max(cities)  # 시작 점, 끝 점
-
-# # 이분 탐색
-# while start <= end:
-#     mid = (start+end) // 2
-#     total = 0  # 총 지출 양
-#     for i in cities:
-#         if i > mid:
-#             total += mid
-#         else:
-#             total += i
-#     if total <= budgets:  # 지출 양이 예산 보다 작으면
-#         start = mid + 1
-#     else:  # 지출 양이 예산 보다 크면
-#         end = mid - 1
-# print(end)
+print(mid_budget)
