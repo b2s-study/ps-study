0a20601f08db42294dba80d7a839a2213790b55b
kingmingseo <multi1971@mju.ac.kr>
2023-01-31 23:47:13 +0900

    [2230] 수 고르기

---
+++ Baekjoon/kingmingseo/[2230] 수 고르기.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+
+N , M = map(int,input().split())
+data = []
+answer = 2147483647
+
+for i in range(N):
+    data.append(int(input()))
+
+start = 0
+end = 0
+data.sort()
+
+while (start < N and end < N):
+    value = data[end] - data[start]
+
+    
+    if value >= M and answer > value:
+        answer = value
+        start= start+1
+
+    elif (value >= M):
+        start = start +1
+
+    else:
+        end= end+1
+print(answer)
