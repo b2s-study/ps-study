b140b9d5cdea04e3470c2766c7b4c27226fb30bc
kingmingseo <multi1971@mju.ac.kr>
2023-02-03 12:50:12 +0900

    [2295] 세 수의 합

--- Baekjoon/kingmingseo/[2295] 세 수의 합.py
+++ Baekjoon/kingmingseo/[2295] 세 수의 합.py
@@ -1,47 +1,23 @@
 import sys
-input = sys.stdin.readline
+input= sys.stdin.readline
 
-n = int(input())
-data =[]
-
-for _ in range(n):
-    data.append(int(input()))
-
-data.sort()
-data_set = set(data)
-
-start = 0
-mid = 0
-end = 0
+loop = int(input())
+two_sum = set()
 answer = 0
-max_value = max(data)
+data = [int(input()) for _ in range(loop)]
+data.sort()
+
+for i in range(loop):
+    for j in range(loop):
+        two_sum.add(data[i]+data[j])
 
 
-while(end < len(data)-1):
-    total = data[start] + data[mid] + data[end]
 
-    if start == mid and mid == end and start == mid and total > max_value:
-        break
+for i in range(loop-1, -1, -1):
+    for j in range(i+1):
+        if data[i]-data[j] in two_sum and answer < data[i]-data[j]  :
+            answer = data[i]
 
-    elif total in data_set and total >= answer :
-        print(start, mid, end)
-        answer = total
-        start = start + 1
-
-
-    elif total not in data_set and total < max_value:
-        start = start + 1
-
-    elif total not in data_set and total > max_value and end < mid:
-        end = mid
-        start = mid
-
-    elif total not in data_set and total > max_value:
-        mid = mid +1
-        start = mid
-
-    else :
-        start = start +1
 
 print(answer)
 
