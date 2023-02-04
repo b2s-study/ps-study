d14c4a89d15df34e8571793951aa0e830d78f5fc
Dongjun Jeong <sam1087@mju.ac.kr>
2023-02-04 17:20:40 +0900

    [2512] 예산 (설명 추가)

--- Baekjoon/dj-1087/[2512] 예산.py
+++ Baekjoon/dj-1087/[2512] 예산.py
@@ -1,10 +1,16 @@
 import sys
 
+# ======================= init ======================= #
+
 input = sys.stdin.readline
 
 N = int(input())
 budget_list = list(map(int, input().split()))
 limit = int(input())
+
+# ======================= solve ======================= #
+# 핵심 로직 - 상한액 이분 탐색
+# 탐색 범위: 0~국가예산(limit)
 
 min_budget, max_budget = 0, max(budget_list)
 while (True):
