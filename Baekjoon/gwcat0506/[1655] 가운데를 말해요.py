b2f5a0a268267dac5a4e8bff6cc555668e2c095a
GounChoi <61350052+gwcat0506@users.noreply.github.com>
2023-02-10 23:48:58 +0900

    가운데를 말해요

---
+++ Baekjoon/gwcat0506/[1655] 가운데를 말해요.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/1655
+
+
+import sys
+import heapq
+import math
+
+N = int(sys.stdin.readline())
+
+# 중앙값보다 작은거 담는 힙
+left_heap = []
+# 중앙값보다 큰거 담는 힙
+right_heap = []
+
+
+        
+        
+
