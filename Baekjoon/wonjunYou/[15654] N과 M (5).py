656f2cfa2141a6fb8d2d043879c50daed7f8f1d2
wonjunYou <youwjune@gmail.com>
2023-03-04 21:08:04 +0900

    [15654] N과 M (5)

---
+++ Baekjoon/wonjunYou/[15654] N과 M (5).py
@@ -0,0 +1,26 @@
+import sys
+
+input = sys.stdin.readline
+
+def func(lens):
+
+    if lens == m:
+        print(*result)
+        return
+
+    for idx in range(len(numbers)):
+        if not visited[idx]:
+            visited[idx] = 1
+            result[lens] = numbers[idx]
+            func(lens + 1)
+            visited[idx] = 0
+
+n, m = map(int, input().rstrip('\n').split())
+numbers = list(map(int, input().rstrip('\n').split()))
+
+numbers.sort()
+
+result = [0] * m
+visited = [0] * (n + 1)
+
+func(0)
