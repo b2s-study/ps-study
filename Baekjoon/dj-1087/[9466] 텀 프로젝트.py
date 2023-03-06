d37e374f5293668187655ae871082fc139ec135d
Dongjun Jeong <sam1087@mju.ac.kr>
2023-03-06 11:32:36 +0900

    [9466] 텀 프로젝트 (해결)

--- Baekjoon/dj-1087/[9466] 텀 프로젝트.py
+++ Baekjoon/dj-1087/[9466] 텀 프로젝트.py
@@ -3,24 +3,18 @@ import sys
 sys.setrecursionlimit(10**6)
 
 
-def matching(first, current, team):
-    team.add(current)
-    members.remove(current)
+def matching(current, team=[]):
+    global selected, visited
+    team.append(current)
+    visited[current] = True
     next = selected[current]
 
-    # 팀 O
-    if next == first:
-        return True
-    # 팀 X
-    if (next in team) or (next not in members):
-        members.add(current)
-        return False
+    if visited[next]:
+        if next in team:
+            return team[team.index(next):]
+        return []
 
-    success = matching(first, next, team)
-    if not success:
-        members.add(current)
-
-    return success
+    return matching(next, team)
 
 
 input = sys.stdin.readline
@@ -29,12 +23,12 @@ T = int(input())
 for _ in range(T):
     n = int(input())
     selected = [-1] + list(map(int, input().split()))
-    members = set(range(1, n+1))
-    remains = len(members)
-    team_members = set()
+    visited = [False] * (n+1)
+    remains = n
 
     for member in range(1, n+1):
-        if member in members:
-            matching(member, member, set())
+        if not visited[member]:
+            team = matching(member, [])
+            remains -= len(team)
 
-    print(len(members))
+    print(remains)
