74d1231bbe010963c2c6df55bc7a4c3d45203745
kingmingseo <multi1971@mju.ac.kr>
2023-03-19 14:17:05 +0900

    [9663] N-QUEEN

---
+++ Baekjoon/kingmingseo/[9663] N-QUEEN.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+ans = 0
+isused1 = [False for _ in range(40)] #같은 열에 있는지
+isused2 = [False for _ in range(40)] # 좌측 하단 우측 상단 대각선
+isused3 = [False for _ in range(40)] # 좌측 상단 우측 하단 대각선
+def func(k):
+    global ans
+    if k == N:
+        ans = ans +1
+        return
+
+    for i in range (N):
+        if (isused1[i] or isused2[i + k] or isused3[k - i + N - 1] ):
+            continue
+        isused1[i] = True
+        isused2[i + k] = True
+        isused3[k - i + N -1] = True
+        func(k + 1)
+        isused1[i] = False
+        isused2[i + k] = False
+        isused3[k - i + N - 1] = False
+
+func(0)
+print(ans)
+
+
+
