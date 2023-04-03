f473837a7488041af7f88172ee5e73819427861c
wonjunYou <youwjune@gmail.com>
2023-04-04 00:04:37 +0900

    [11404] 플로이드 수정

--- Baekjoon/wonjunYou/[11404] 플로이드.py
+++ Baekjoon/wonjunYou/[11404] 플로이드.py
@@ -20,14 +20,14 @@ for i in range(m):
     if cost[a][b] > c:
         cost[a][b] = c
 
-for k in range(N + 1):
-    for i in range(N + 1):
-        for j in range(N + 1):
+for k in range(1, N + 1):
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
             cost[i][j] = min(cost[i][j],cost[i][k] + cost[k][j])
 
 for i in range(1, N + 1):
     for j in range(1, N + 1):
-        if cost[i][j] == int(1e9):
+        if cost[i][j] == INF:
             print(0, end=' ')
         else:
             print(cost[i][j],end=' ')
